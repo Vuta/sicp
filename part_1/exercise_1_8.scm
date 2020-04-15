d869465f6a132e0d3e2ce4a746bdde6dff325d47
Tran Anh Vu <eyelight93@gmail.com>
2020-04-15 22:47:13 +0700

    exercise 1.8

---
+++ part_1/exercise_1_8.scm
@@ -0,0 +1,29 @@
+(define (abs x)
+  (if (> x 0) x (- x))
+)
+
+(define (square x)
+  (* x x)
+)
+
+(define (good-enough? current_guess previous_guess)
+  (< (abs (/ (- current_guess previous_guess) current_guess)) 0.00000000001)
+)
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3)
+)
+
+(define (sqrt-iter y guess)
+  (define current_guess (improve guess y))
+  (if (good-enough? current_guess guess)
+    current_guess
+    (sqrt-iter y current_guess)
+  )
+)
+
+(define (sqrt y)
+  (sqrt-iter y 1.0)
+)
+
+(sqrt 3)
