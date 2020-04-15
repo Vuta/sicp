9277b8472ad1000653843ef856d639e5ffd3625f
Tran Anh Vu <eyelight93@gmail.com>
2020-04-15 23:26:21 +0700

    Refactor square root

--- part_1/square_root.scm
+++ part_1/square_root.scm
@@ -2,24 +2,19 @@
   (if (> x 0) x (- x))
 )
 
-(define (good-enough? current_guess previous_guess)
-  (< (abs (/ (- current_guess previous_guess) current_guess)) 0.00000000001)
-)
-
-(define (improve guess y)
-  (/ (+ guess (/ y guess)) 2) 
-)
-
-(define (sqrt-iter y guess)
-  (define current_guess (improve guess y))
-  (if (good-enough? current_guess guess)
-    current_guess
-    (sqrt-iter y current_guess)
-  )
-)
-
 (define (sqrt y)
-  (sqrt-iter y 1.0)
+  (define (good-enough? current_guess previous_guess)
+    (< (abs (/ (- current_guess previous_guess) current_guess)) 0.00000000001)
+  )
+  (define (improve guess y)
+    (/ (+ guess (/ y guess)) 2)
+  )
+  (define (sqrt-iter guess)
+    (define current_guess (improve guess y))
+    (if (good-enough? current_guess guess)
+      current_guess
+      (sqrt-iter current_guess)
+    )
+  )
+  (sqrt-iter 1.0)
 )
-
-(sqrt 2)
