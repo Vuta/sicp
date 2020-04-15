721df126e086bf2a6d44651c18a5dd3018dfc559
Tran Anh Vu <eyelight93@gmail.com>
2020-04-15 22:39:51 +0700

    exercise 1.7

--- part_1/square_root.scm
+++ part_1/square_root.scm
@@ -2,8 +2,8 @@
   (if (> x 0) x (- x))
 )
 
-(define (good-enough? y guess)
-  (< (abs (- (* guess guess) y)) 0.001)
+(define (good-enough? current_guess previous_guess)
+  (< (abs (/ (- current_guess previous_guess) current_guess)) 0.00000000001)
 )
 
 (define (improve guess y)
@@ -11,14 +11,15 @@
 )
 
 (define (sqrt-iter y guess)
-  (if (good-enough? y guess)
-    guess
-    (sqrt-iter y (improve guess y))
+  (define current_guess (improve guess y))
+  (if (good-enough? current_guess guess)
+    current_guess
+    (sqrt-iter y current_guess)
   )
 )
 
 (define (sqrt y)
-  (sqrt-iter y 1)
+  (sqrt-iter y 1.0)
 )
 
 (sqrt 2)
