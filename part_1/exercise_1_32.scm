572220df26de9bb08a1098ce859d3137cc89eb23
Tran Anh Vu <eyelight93@gmail.com>
2020-04-17 17:57:07 +0700

    exercise 1.32

---
+++ part_1/exercise_1_32.scm
@@ -0,0 +1,19 @@
+(define (identity a) a)
+(define (inc a) (+ a 1))
+
+(define (accu combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner (term a) result))
+    )
+  )
+
+  (iter a null-value)
+)
+
+(define (product term a next b)
+  (accu * 1 term a next b)
+)
+
+(product identity 1 inc 5)
