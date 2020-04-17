ad9d7fcd5009c0bc59e59514f41fa19402582f79
Tran Anh Vu <eyelight93@gmail.com>
2020-04-17 21:19:04 +0700

    add square root by average damping

--- part_1/fixed_point.scm
+++ part_1/fixed_point.scm
@@ -1,3 +1,7 @@
+(define (average x y)
+  (/ (+ x y) 2.0)
+)
+
 (define (fix-point f init-guess)
   (define (good-enough? x y) (< (abs (- x y)) 0.00001))
   (define (try guess)
@@ -14,3 +18,9 @@
 
 (fix-point cos 1.0)
 (fix-point (lambda (y) (+ (sin y) (cos y))) 1.0)
+
+(define (sqrt n)
+  (fix-point (lambda (x) (average x (/ n x))) 1.0)
+)
+
+(sqrt 2)
