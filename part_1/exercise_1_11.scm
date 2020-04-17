78b8bfa067c0c3bef44037bcb5708740d2e2d635
Tran Anh Vu <eyelight93@gmail.com>
2020-04-17 07:14:24 +0700

    exercise 1.11 iterative

--- part_1/exercise_1_11.scm
+++ part_1/exercise_1_11.scm
@@ -6,3 +6,15 @@
 )
 
 (f-recur 10)
+
+(define (f-iter n)
+  (define (iter a b c counter)
+    (if (= counter 0)
+       a
+       (iter b c (+ c (* 2 b) (* 3 a)) (- counter 1))
+    )
+  )
+  (iter 0 1 2 n)
+)
+
+(f-iter 10)
