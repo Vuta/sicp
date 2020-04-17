9d78f9cccaf0df09ca4e4536b80da9524ab913c4
Tran Anh Vu <eyelight93@gmail.com>
2020-04-17 07:55:10 +0700

    exponential iterative

--- part_1/exponentiation.scm
+++ part_1/exponentiation.scm
@@ -8,3 +8,15 @@
 )
 
 (expo 2 3)
+
+(define (expo-iter a n)
+  (define (iter a counter product)
+    (if (= counter 0)
+        product
+        (iter a (- counter 1) (* a product))
+    )
+  )
+  (iter a n 1)
+)
+
+(expo-iter 2 4)
