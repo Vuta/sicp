76a82e3282fd6d5f2d8689fabd8d520470353468
Tran Anh Vu <eyelight93@gmail.com>
2020-04-19 09:58:52 +0700

    rational number

---
+++ part_2/rational_number.scm
@@ -0,0 +1,42 @@
+(define (gcd n d)
+  (if (= d 0)
+      n
+      (gcd d (remainder n d))))
+
+(define (make-rat n d)
+  (let ((a (gcd n d)))
+    (cons (/ n a) (/ d a))))
+
+(define (numer x)
+  (car x))
+
+(define (denom x)
+  (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (numer y) (denom x))))
+
+(define (equal-rat x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
