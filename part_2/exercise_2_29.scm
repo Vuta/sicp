86d9b16150c4ffec4cb975637169028898627dde
Tran Anh Vu <eyelight93@gmail.com>
2020-04-20 19:50:44 +0700

    exercise 2.29b

--- part_2/exercise_2_29.scm
+++ part_2/exercise_2_29.scm
@@ -5,7 +5,19 @@
   (list length structure))
 
 (define (left-br mobile) (car mobile))
-(define (right-br mobile) (cdr mobile))
+(define (right-br mobile) (cadr mobile))
 
 (define (br-length br) (car br))
-(define (br-structure br) (cdr br))
+(define (br-structure br) (cadr br))
+
+(define (total-weight mobile)
+  (cond ((null? mobile) 0)
+        ((not (pair? mobile)) mobile)
+        (else
+         (+
+          (total-weight (br-structure (left-br mobile)))
+          (total-weight (br-structure (right-br mobile)))
+         )
+        )
+  )
+)
