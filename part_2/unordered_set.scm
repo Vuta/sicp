6190c8ff25f2294595342ba26e329233b2fd286c
Tran Anh Vu <eyelight93@gmail.com>
2020-04-28 16:37:00 +0700

    exercise 2.59

--- part_2/unordered_set.scm
+++ part_2/unordered_set.scm
@@ -19,3 +19,12 @@
         (else (intersection-set (cdr s1) s2))
   )
 )
+
+(define (union-set s1 s2)
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        ((element-of-set? s2 (car s1))
+          (union-set (cdr s1) s2))
+        (else (cons (car s1) (union-set (cdr s1) s2)))
+  )
+)
