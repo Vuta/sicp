15602da356d9b49f7b93c335a715f07705d3a241
Tran Anh Vu <eyelight93@gmail.com>
2020-04-28 17:43:12 +0700

    adjoin order set

--- part_2/ordered_set.scm
+++ part_2/ordered_set.scm
@@ -17,3 +17,11 @@
       )
   )
 )
+
+(define (adjoin-set set x)
+  (cond ((null? set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set) (adjoin-set (cdr set)  x)))
+  )
+)
