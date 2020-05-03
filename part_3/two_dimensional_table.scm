ea42661d64df7156c56f76141b392b32b10b10c0
Tran Anh Vu <eyelight93@gmail.com>
2020-05-03 08:18:05 +0700

    lookup 2 dimensional table

---
+++ part_3/two_dimensional_table.scm
@@ -0,0 +1,13 @@
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (lookup key-1 key-2 table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable 
+        (let ((record (assoc key-2 (cdr subtable))))
+          (if record
+              (cdr record)
+              false))
+        false)))
