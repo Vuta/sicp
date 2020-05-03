85850a1cfd4e30de4644c71f59861e781978455a
Tran Anh Vu <eyelight93@gmail.com>
2020-05-04 01:34:12 +0700

    inverter

---
+++ part_3/inverter.scm
@@ -0,0 +1,11 @@
+(define (logical-not signal)
+  (cond ((= signal 0) 1)
+        ((= signal 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay (lambda () (set-signal! ouput new-value)))))
+  (add-action! input invert-input)
+  'ok)
