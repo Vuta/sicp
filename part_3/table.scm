3da5a5e65ccf7cd4335dcda3462e4cfad2f80adb
Tran Anh Vu <eyelight93@gmail.com>
2020-05-03 08:03:10 +0700

    insert table

--- part_3/table.scm
+++ part_3/table.scm
@@ -8,3 +8,10 @@
     (if record
         (cdr record)
         false)))
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+        (set-cdr! table (cons (cons key value) (cdr table)))))
+  'ok)
